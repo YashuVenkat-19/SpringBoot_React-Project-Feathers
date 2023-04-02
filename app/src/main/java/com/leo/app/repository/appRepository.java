1340aca9aabd6541130c41a8821bc1472b39e704
Yashu Venkat Sivalingam <95931116+YashuVenkat-19@users.noreply.github.com>
2023-04-02 22:34:08 +0530

    Delete app directory

--- app/src/main/java/com/leo/app/repository/appRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leo.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.leo.app.entity.App;
-
-public interface appRepository extends JpaRepository<App, Integer> {
-
-}
