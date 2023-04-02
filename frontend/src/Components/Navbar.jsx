b2aedc1602493e98a4f45135a73734bad8679830
Yashu Venkat Sivalingam <95931116+YashuVenkat-19@users.noreply.github.com>
2023-04-02 22:34:28 +0530

    Delete frontend directory

--- frontend/src/Components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <div className="bg-gray-800">
-      <div className="h-16 px-8 flex items-center">
-        <p className="text-white font-bold">Institute Management System</p>
-      </div>
-    </div>
-  );
-}
